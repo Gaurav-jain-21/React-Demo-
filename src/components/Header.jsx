4748e627775a68a1482841886f7043411b0ad41f
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-01 22:06:11 +0530

    this is just for learning

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1> Welcom to my Web site</h1>
+        <nav>
+            <a href="">Home</a><a href="">About</a><a href="">Contact</a>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Header
