38601c2438ce773b884c344d0449d0a8129a8a28
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-05 00:25:41 +0530

    changing the directory path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Greet from "./components/Greet";
-import Expressions from "./components/Expressions";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Arrays from "./components/Arrays";
-import ProductInfo from "./components/ProductInfo";
-import PropsEx from "./components/PropsEx";
+import Greet from "./components/Old_playlist/Greet";
+import Expressions from "./components/Old_playlist/Expressions";
+import Footer from "./components/Old_playlist/Footer";
+import Header from "./components/Old_playlist/Header";
+import Main from "./components/Old_playlist/Main";
+import Arrays from "./components/Old_playlist/Arrays";
+import ProductInfo from "./components/Old_playlist/ProductInfo";
+import PropsEx from "./components/Old_playlist/PropsEx";
 const App = () => {
   return <div>
 
