6f969b8f8a1f3ded1fb361f33cec7c56328d302e
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-09 21:39:27 +0530

    this my list page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Greet from "./component1/Greet.jsx";
 import Header from "./component1/Header.jsx";
 import Main from "./component1/Main.jsx";
 import Footer from "./component1/Footer.jsx";
+import List1 from "./component1/List1.jsx";
 import Expression from "./component1/Expression.jsx";
 const App=()=>{
   return <div>
@@ -10,6 +11,7 @@ const App=()=>{
     <Main/>
     <Footer/>
     <Expression/>
+    <List1/>
   </div>
 }
 export default App;
