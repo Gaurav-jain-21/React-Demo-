0e9aefdb3cf89219838b617701103f93ea871a12
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-09 21:13:45 +0530

    this is my first practices

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Greet from "./component1/Greet.jsx";
+import Header from "./component1/Header.jsx";
+import Main from "./component1/Main.jsx";
+import Footer from "./component1/Footer.jsx";
 const App=()=>{
   return <div>
+    <Header/>
     <Greet/>
+    <Main/>
+    <Footer/>
   </div>
 }
 export default App;
---
+++ src/component1/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <p>@ 2025 my Website</p>
+    </div>
+  )
+}
+
+export default Footer
