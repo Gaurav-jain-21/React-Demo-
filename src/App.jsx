8b42a2a11308cf98eb5a934954772fdc30828fff
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-11 14:45:17 +0530

    this is my page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import ProfileCard from "./component1/ProfileCard.jsx";
 import ContectDetail from "./ContectDetail.jsx";
+import CopyInput from "./component1/CopyInput.jsx";
 const App=()=>{
   return <div>
     <ProfileCard/>
     <br>
     </br>
     <ContectDetail/>
+    <br/>
+    <CopyInput/>
   </div>
 }
 export default App;
