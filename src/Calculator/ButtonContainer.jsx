f7ee9fa9baf0ed42abad209b6cf33aa7f46c7e7d
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-13 22:22:11 +0530

    this is my calculator ui

--- src/Calculator/ButtonContainer.jsx
+++ src/Calculator/ButtonContainer.jsx
@@ -1,11 +1,29 @@
 import styles from "./ButtonContainer.module.css";
 const ButtonContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <div className={styles.buttonsContainer}>
-      <button className={styles.buttons}>C</button>
-      <button className={styles.buttons}>1</button>
-      <button className={styles.buttons}>2</button>
-      <button className={styles.buttons}>+</button>
+      {buttonNames.map((buttonName) => (
+        <button className={styles.buttons}>{buttonName}</button>
+      ))}
     </div>
   );
 };
