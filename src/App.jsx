fad7460f607481b2732653546bb73a09a7148faf
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-13 22:05:00 +0530

    this is my page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import Food from "./projects/Food.jsx";
+import styles from "./App.module.css";
+import Display from "./Calculator/Display.jsx";
 
-const App=()=>{
-  return <div>
-    <Food/>
-  </div>
-}
+const App = () => {
+  return (
+    <div className={styles.calculator}>
+      <Display/>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.buttons}>C</button>
+        <button className={styles.buttons}>1</button>
+        <button className={styles.buttons}>2</button>
+        <button className={styles.buttons}>+</button>
+      </div>
+    </div>
+  );
+};
 export default App;
