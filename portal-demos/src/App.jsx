0a3a7403fba0bbc35e25f28ac435a36f1c9fab36
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-02-02 23:29:06 +0530

    this is my switcher

--- portal-demos/src/App.jsx
+++ portal-demos/src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import CopyInput from "./components/CopyInput.jsx";
+import Switcher from "./components/Switcher.jsx";
 const App = () => {
   return (
     <div>
       <CopyInput></CopyInput>
+      <br />
+      <Switcher />
     </div>
   );
 };
