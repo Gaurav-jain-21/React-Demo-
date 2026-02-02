5647bc0fb071e2d905ce973f8b73f328ff79ef8b
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-02-02 23:48:56 +0530

    this is my basic initila of useEffect

---
+++ hooks-learning2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+const App = () => {
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("Call useEffect");
+    document.title = `Increment ${value}`;
+  }, []); //if initialize empty array this function only fire onece
+  return (
+    <div>
+      <h2>{value}</h2>
+      <button onClick={() => setValue(value + 1)}>Click</button>
+    </div>
+  );
+};
+export default App;
