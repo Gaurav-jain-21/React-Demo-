092108201bdd01162b5497dfc421826ddcd903d2
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-02-02 15:26:59 +0530

    this is my basic todo List

--- hooks-learning/src/App.jsx
+++ hooks-learning/src/App.jsx
@@ -1,20 +1,8 @@
-import Example1 from "./components/Example1.jsx";
-import Example2 from "./components/Example2.jsx";
-import Example3 from "./components/Example3.jsx";
-import Example4 from "./components/Example4.jsx";
-import Example5 from "./components/Example5.jsx";
+import Todo from "./components/Todo.jsx";
 const App = () => {
   return (
     <>
-      <Example1 />
-      <br />
-      <Example2 />
-      <br />
-      <Example3 />
-      <br />
-      <Example4 />
-      <br />
-      <Example5 />
+      <Todo />
     </>
   );
 };
---
+++ hooks-learning/src/components/Todo.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+const Todo = () => {
+  const [name, setName] = useState(["ram", "vikash", "ashif", "rahul"]);
+  const [new_name, setNewName] = useState("");
+  const handleChange1 = (event) => {
+    setNewName(event.target.value);
+  };
+  const handleAddName = () => {
+    setName([...name, new_name]);
+    setNewName("");
+  };
+  const handleDelete = (n) => {
+    setName(name.filter((f) => f !== n));
+  };
+  return (
+    <section>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        onChange={handleChange1}
+      ></input>
+      <button onClick={handleAddName}>Add Name</button>
+      <h3>Todo List</h3>
+      <ul>
+        {name.map((n, index) => (
+          <li key={index}>
+            <div>
+              <h4>{n}</h4>
+              <button value={n} onClick={() => handleDelete(n)}>
+                Delete name
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+export default Todo;
