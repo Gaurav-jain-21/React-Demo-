6c93e20d053d64498c75ad881682da7b5c86f278
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-02-02 23:16:28 +0530

    this is my first copy function

---
+++ portal-demos/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CopyInput from "./components/CopyInput.jsx";
+const App = () => {
+  return (
+    <div>
+      <CopyInput></CopyInput>
+    </div>
+  );
+};
+
+export default App;
